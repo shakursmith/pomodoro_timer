b9d439a89ea46d04c6ddd42cf5330e632dd0ace0
Shakur Smith <smith.shakur@ymail.com>
2021-02-01 21:53:19 -0600

    build for React

---
+++ build/precache-manifest.4b7a182869d7cfd6760f134392f295b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f414cedfdba9bf73d1ad4dc56ea121",
+    "url": "/pomodoro_timer/index.html"
+  },
+  {
+    "revision": "6f2801df0e61a6e1247c",
+    "url": "/pomodoro_timer/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "226820f6af32e486a61f",
+    "url": "/pomodoro_timer/static/js/2.0885f580.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pomodoro_timer/static/js/2.0885f580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f2801df0e61a6e1247c",
+    "url": "/pomodoro_timer/static/js/main.2dc2e7f0.chunk.js"
+  },
+  {
+    "revision": "7026a246fac58476e26f",
+    "url": "/pomodoro_timer/static/js/runtime-main.6015f5ea.js"
+  }
+]);
